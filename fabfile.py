347788038763225a0b1113210a088c75270d06db
Alex Dorey <dorey415@gmail.com>
2011-03-02 17:54:50 +0530

    checking for local changes on repositories before 'git-pull'ing them in the fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import local
+from fabric.api import local, cd
 import os
 
 DJANGROVE_DIR = os.path.abspath('.')
@@ -15,12 +15,24 @@ def git_pull_all():
     Clones/Pulls the develop branch of the mangrove subprojects
     necessary to get djangrove running.
     """
+    dirty_repos = []
     for app_name in ACTIVE_APPS:
         if not os.path.exists( \
                 os.path.join(DJANGROVE_DIR, app_name)):
             local("git clone -b develop %s" % APP_REPOS[app_name])
-#        else:
-#            local("cd %s && git pull origin develop" % app_name)
+    else:
+        with cd(app_name):
+            #a kinda dirty way of checking if there are local changes...
+            status = local("git status | grep -qF 'working directory clean' || echo 'dirty'")            
+            if status=="dirty":
+                dirty_repos.append(app_name)
+            else:
+                print "Pulling most recent changes %s" % app_name
+                print local("git pull origin develop")
+    if len(dirty_repos) > 0:
+        dirty_repo_list = ", ".join(dirty_repos)
+        raise Exception("Local changes on %d repository/repositories:\n%s" % \
+                            (len(dirty_repos), dirty_repo_list))
 
 def setup_project():
     """
