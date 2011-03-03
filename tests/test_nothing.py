d18a4a061797e5bc9144b32aead5d820ab606a99
Alex Dorey <dorey415@gmail.com>
2011-03-03 12:49:17 +0530

    testing how nosetests runs tests.

--- tests/test_nothing.py
+++ tests/test_nothing.py
@@ -8,7 +8,13 @@ import sys
 import os
 
 class TestExample:
+    def setUp(self):
+        self.test_value = 3
+    
 	def test_abb(self):
 		assert 'abb'=='abb'
 	def test_true(self):
 		assert True
+	
+	def test_setup_is_called_before_tests(self):
+	    assert self.test_value==3
