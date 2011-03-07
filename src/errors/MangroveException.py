ff1c3935e297a787a27656bd44720cb05322a0e5
aviars <aviars@videntity.com>
2011-03-07 01:13:49 -0500

    Added a error exception module and documentation for the team's consideration

---
+++ src/errors/MangroveException.py
@@ -0,0 +1,10 @@
+class MangroveException(Exception): pass
+class DataDictionaryFormatException(MangroveException):pass
+class StoreException(MangroveException):pass
+class FieldValidationException(MangroveException):pass
+class FormValidationException(MangroveException):pass
+class FormBuilderException(MangroveException):pass
+class SMSGatewayException(MangroveException):pass
+class MessageAPIException(MangroveException):pass
+class VUMIException(MangroveException):pass
+class AuthenticationException(MangroveException):pass
