f07d4177f87a6c03200538e944060042df394e2c
Akshay Naval <akshaysn@akshaysn-Latitude-E6500.(none)>
2011-04-01 18:25:50 +0530

    Reading all map and reduce function from a js file(just a start, Will move this code to src soon) :Akshay/Mahesh

---
+++ src/services/entity_management/CouchViews/map_by_location.js
@@ -0,0 +1,25 @@
+function(doc) {
+    var isNumeric = function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+    var isNotNull = function(o) {
+        return !((o === undefined) || (o == null));
+    };
+    if (doc.document_type == 'DataRecord' && isNotNull(doc.entity_backing_field)) {
+        var value = {};
+        var date = new Date(doc.created_on);
+        var aggregation_trees = doc.entity_backing_field.aggregation_trees || {};
+        for (index in doc.attributes) {
+            if (isNotNull(doc.attributes[index]) && isNotNull(doc.attributes[index]['value']) && isNumeric(doc.attributes[index]['value'])) {
+                value[index] = parseFloat(doc.attributes[index]['value']);
+            }
+        }
+        for (index in value) {
+            for (hierarchy in aggregation_trees) {
+                var key = [index].concat([hierarchy], aggregation_trees[hierarchy], [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()]);
+                key.splice(0, 0, doc.entity_backing_field.entity_type);
+                emit(key, value[index]);
+            }
+        }
+    }
+}
--- src/services/entity_management/entity_management_service.py
+++ src/services/entity_management/entity_management_service.py
@@ -1,6 +1,9 @@
+import os
 from services.entity_management.models import  Entity
 from services.repository.connection import Connection
 from services.repository.repository import Repository
+
+file_path = os.path.dirname(__file__)
 
 class EntityManagementService:
 
@@ -61,134 +64,24 @@ class EntityManagementService:
         return self.repository.save(entity, entity.__class__)
 
     def __create_by_location_view(self):
-
-        map = """ function(doc)
-                 {
-                   var isNumeric = function(n)
-                   {
-                                       return !isNaN(parseFloat(n)) && isFinite(n);
-                   };
-                   var isNotNull = function(o)
-                   {
-                        return !((o === undefined) || (o == null));
-                   };
-                   if (doc.document_type == 'DataRecord' && isNotNull(doc.entity_backing_field))
-                   {
-                       var value = {};
-                       var date = new Date(doc.created_on);
-                       var aggregation_trees = doc.entity_backing_field.aggregation_trees || {};
-                       for(index in doc.attributes)
-                       {
-                            if(isNotNull(doc.attributes[index]) && isNotNull(doc.attributes[index]['value']) && isNumeric(doc.attributes[index]['value']))
-                            {
-                                value[index] = parseFloat(doc.attributes[index]['value']);
-                            }
-                        }
-                        for(index in value)
-                        {
-                           for(hierarchy in aggregation_trees)
-                           {
-                               var key = [index].concat([hierarchy], aggregation_trees[hierarchy], [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()]);
-                               key.splice(0,0, doc.entity_backing_field.entity_type);
-                               emit(key ,value[index]);
-                           }
-                        }
-                    }
-                 }"""
-        reduce = """ _stats """
+        path = file_path+"/CouchViews/map_by_location.js"
+        map = """""".join(["%s"% line for line in open(path).readlines()])
+        path = file_path+"/CouchViews/reduce_by_location.js"
+        reduce = """""".join(["%s"% line for line in open(path).readlines()])
         self.repository.create_view("by_location",map,reduce)
 
     def __create_by_time_view(self):
-
-        map = """  function(doc)
-                  {
-                    var isNumeric = function(n)
-                    {
-                        return !isNaN(parseFloat(n)) && isFinite(n);
-                    };
-                    var isNotNull = function(o)
-                    {
-                        return !((o === undefined) || (o == null));
-                    };
-                    if (doc.document_type == 'DataRecord' && isNotNull(doc.entity_backing_field))
-                    {
-                        var value = {};
-                        var date = new Date(doc.created_on);
-                        for(index in doc.attributes)
-                        {
-			            if(isNotNull(doc.attributes[index]) && isNotNull(doc.attributes[index].value) && isNumeric(doc.attributes[index].value))
-                        {
-                            value[index] = parseFloat(doc.attributes[index].value);
-                        }
-                     }
-			         for(index in value)
-                         {
-                            var key = [doc.entity_backing_field.entity_type, index, date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
-                            emit(key, value[index]);
-                         }
-                     }
-                  }"""
-        reduce = """ _stats """
+        path = file_path+"/CouchViews/map_by_time.js"
+        map = """""".join(["%s"% line for line in open(path).readlines()])
+        path = file_path+"/CouchViews/reduce_by_time.js"
+        reduce = """""".join(["%s"% line for line in open(path).readlines()])
         self.repository.create_view("by_time",map,reduce)
 
     def __create_current_values_view(self):
-        map = """  function(doc)
-                 {
-                    var isNotNull = function(o)
-                    {
-                        return !((o === undefined) || (o == null));
-                    };
-                    if (doc.document_type == 'DataRecord' && isNotNull(doc.entity_backing_field))
-                    {
-                        var value = {entity_type: { value:doc.entity_backing_field.entity_type }, document_type: { value:doc.entity_backing_field.document_type}};
-                        var date = new Date(doc.created_on);
-                        if(isNotNull(doc.entity_backing_field) && isNotNull(doc.entity_backing_field.attributes))
-                        {
-                            var attributes = doc.entity_backing_field.attributes;
-                            for(index in attributes)
-                            {
-                                 if(isNotNull(attributes[index]) && isNotNull(attributes[index]['value']))
-                                 {
-                                      var attribute_object = attributes[index];
-                                      attribute_object['timestamp_for_view'] = date.getTime();
-                                      value[index] = attributes[index];
-                                 }
-                            }
-                        }
-                        for(index in doc.attributes)
-                        {
-                             var attributes = doc.attributes;
-                             if(isNotNull(attributes[index]) && isNotNull(attributes[index]['value']))
-                             {
-                                 var attribute_object = attributes[index];
-                                 attribute_object['timestamp_for_view'] = date.getTime();
-                                 value[index] = attributes[index];
-                             }
-                         }
-                         var key = [doc.entity_backing_field.entity_type, doc.entity_backing_field._id, date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
-                         emit (key, value);
-                     }
-                }"""
-        reduce = """function(key, values, rereduce){
-                            var isNull = function(o)
-                            {
-                                return (o === undefined) || (o == null);
-                            };
-
-                            var current = { entity_id : {value: key[0][0][1] } };
-
-                            for(value in values)
-                            {
-                                for(index in values[value])
-                                {
-                                    if(isNull(current[index]) || values[value][index].timestamp_for_view > current[index].timestamp_for_view)
-                                    {
-                                        current[index] = values[value][index];
-                                    }
-                                }
-                            }
-                            return current;
-                    }"""
+        path = file_path+"/CouchViews/map_current_values.js"
+        map = """  """.join(["%s"% line for line in open(path).readlines()])
+        path = file_path+"/CouchViews/reduce_current_values.js"
+        reduce = """  """.join(["%s"% line for line in open(path).readlines()])
         self.repository.create_view("current_values",map,reduce)
 
     def exists_view(self, aggregation):
