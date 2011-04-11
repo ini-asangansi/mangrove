9016b5e103bf5b971e3c53acb69384ad8ef1d470
Jeff Wishnie <jeff@wishnie.org>
2011-04-11 09:38:43 -0700

    replacing test that I somehow removed

--- src/datastore/views/__init__.py
+++ src/datastore/views/__init__.py
@@ -5,19 +5,17 @@ from glob import iglob
 import string
 
 def create_views(dbm):
-    """
-    Creates a standard set of views in the database
-    """
+    '''Creates a standard set of views in the database'''
     global view_js
     database_manager = dbm
     for v in view_js.keys():
-        if not exists_view(v,database_manager):
+        if not exists_view(v, database_manager):
             funcs = view_js[v]
             map = (funcs['map'] if 'map' in funcs else None)
             reduce = (funcs['reduce'] if 'reduce' in funcs else '_stats')
             database_manager.create_view(v, map, reduce)
 
-def exists_view(aggregation,database_manager):
+def exists_view(aggregation, database_manager):
     entity_type_views = database_manager.load('_design/mangrove_views')
     if entity_type_views is not None and entity_type_views['views'].get(aggregation):
         return True
@@ -25,7 +23,7 @@ def exists_view(aggregation,database_manager):
 
 def find_views():
     views = {}
-    for fn in iglob(os.path.join(os.path.dirname(__file__),'*.js')):
+    for fn in iglob(os.path.join(os.path.dirname(__file__), '*.js')):
         try:
             func, name = string.split(os.path.splitext(os.path.basename(fn))[0],'_',1)
             with open(fn) as f:
