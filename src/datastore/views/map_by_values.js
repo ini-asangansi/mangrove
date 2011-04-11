90f9d4cd6d2c32e2ab534c0ef228ad017401a06c
diptanu <admin.nitjece@gmail.com>
2011-04-11 22:22:28 +0530

    Fixing a view for mac platforms and hopefully not break other platforms :-)

--- src/datastore/views/map_by_values.js
+++ src/datastore/views/map_by_values.js
@@ -1,14 +1,17 @@
 function(doc) {
- 
-
-for (k in doc.data){
-	var date = new Date(doc.event_time);
-	key = [doc.entity_backing_field.aggregation_paths['_type'], 
-	doc.entity_backing_field._id,k,date.getUTCFullYear(), date.getUTCMonth() + 1,
-	date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()];
-	doc.data[k]["timestamp"] = date.getTime()
-	emit(key, doc.data[k]);
-}
+  
+  
+  for (k in doc.data){
+    value = {};
+    var date = new Date(doc.event_time);
+    key = [doc.entity_backing_field.aggregation_paths['_type'], 
+	   doc.entity_backing_field._id,k,date.getUTCFullYear(), date.getUTCMonth() + 1,
+	   date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()];
+    value["timestamp"] = date.getTime();
+    value["type"] = doc.data[k].type;
+    value["value"] = doc.data[k].value;
+    emit(key, value);
+  }
 
 
 }
