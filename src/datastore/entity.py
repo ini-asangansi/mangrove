b995fad120b1fe18fc7ce1340e5a09f48250145d
diptanu <admin.nitjece@gmail.com>
2011-04-11 17:59:07 +0530

    Revert "removed unneeded commented out code"
    This reverts commit d40e512529320f825faadc3009720ca01f5a271d.

--- src/datastore/entity.py
+++ src/datastore/entity.py
@@ -1,6 +1,6 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-
 import copy
+
 from datetime import datetime
 from documents import EntityDocument, DataRecordDocument, attributes
 
@@ -139,8 +139,8 @@ class Entity(object):
     @property
     def aggregation_paths(self):
         '''Returns a copy of the dict'''
+#        return dict(self._doc.aggregation_paths)
         return copy.deepcopy(self._doc.aggregation_paths)
-    
     @property
     def type_path(self):
         '''Returns a copy of the path'''
