73f57ac0480d71049cf7ca3772d7cb728c116162
Jeff Wishnie <jeff@wishnie.org>
2011-04-11 00:44:11 -0700

    full iso format in Couch

--- src/datastore/views/map_by_values.js
+++ src/datastore/views/map_by_values.js
@@ -4,8 +4,8 @@ function(doc) {
 for (k in doc.data){
 	var date = new Date(doc.event_time);
 	key = [doc.entity_backing_field.aggregation_paths['_type'], 
-	doc.entity_backing_field._id,k,date.getFullYear(), date.getMonth() + 1, 
-	date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()];
+	doc.entity_backing_field._id,k,date.getUTCFullYear(), date.getUTCMonth() + 1,
+	date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()];
 	doc.data[k]["timestamp"] = date.getTime()
 	emit(key, doc.data[k]);
 }
