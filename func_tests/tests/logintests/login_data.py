cb08ef41fa59e4e4638e9806aa1f426adb869089
Ritesh-Yadav <yadavr@thoughtworks.com>
2011-05-06 16:15:19 +0530

    Implemented PEP8 in login related functional tests

--- func_tests/tests/logintests/login_data.py
+++ func_tests/tests/logintests/login_data.py
@@ -1,6 +1,5 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
-__author__ = 'ritesh'
 
 USERNAME = 'username'
 PASSWORD = 'password'
@@ -8,30 +7,32 @@ WELCOME_MESSAGE = 'message'
 ERROR_MESSAGE = 'message'
 
 # valid credentials
-VALID_CREDENTIALS={"username":"tester150411@gmail.com",
-                   "password":"tester150411",
-                       "message":"Welcome Mr. Tester Pune"}
+VALID_CREDENTIALS = {"username": "tester150411@gmail.com",
+                   "password": "tester150411",
+                       "message": "Welcome Mr. Tester Pune"}
 # invalid format email id
-INVALID_EMAIL_ID_FORMAT={"username":"com.invalid@mail","password":"nogo123",
-                       "message":"Please enter a correct email and password."}
+INVALID_EMAIL_ID_FORMAT = {"username": "com.invalid@mail",
+                           "password": "nogo123",
+                       "message": "Please enter a correct email and password."}
 # invalid password
-INVALID_PASSWORD={"username":"nogo@mail.com","password":"nogo124",
-                  "message":"Please enter a correct email and password."}
+INVALID_PASSWORD = {"username": "nogo@mail.com", "password": "nogo124",
+                  "message": "Please enter a correct email and password."}
 
 # Login without entering Email Address
-BLANK_EMAIL_ADDRESS={"username":"","password":"nogo123",
-                     "message":"Email  This field is required."}
+BLANK_EMAIL_ADDRESS = {"username": "", "password": "nogo123",
+                     "message": "Email  This field is required."}
 
-BLANK_PASSWORD={"username":"nogo@mail.com","password":"",
-                "message":"Password  This field is required."}
-
-
-# blank username and password
-UNACTIVATED_ACCOUNT_CREDENTIALS={"username":"tester@gmail.com",
-                                 "password":"nogo123",
-                   "message":"This account is inactive."}
+BLANK_PASSWORD = {"username": "nogo@mail.com", "password": "",
+                "message": "Password  This field is required."}
 
 
 # blank username and password
-BLANK_CREDENTIALS={"username":"","password":"",
-                   "message":"Email  This field is required.Password  This field is required."}
+UNACTIVATED_ACCOUNT_CREDENTIALS = {"username": "tester@gmail.com",
+                                 "password": "nogo123",
+                   "message": "This account is inactive."}
+
+
+# blank username and password
+BLANK_CREDENTIALS = {"username": "", "password": "",
+                   "message": "Email  This field is required.\
+                   Password  This field is required."}
