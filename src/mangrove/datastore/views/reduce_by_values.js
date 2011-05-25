f0693be8990198984d69e72638ac6153af09b70d
Aroj George <apgeorge@thoughtworks.com>
2011-05-25 16:49:14 +0530

    Trying to split out latest values

--- src/mangrove/datastore/views/reduce_by_values.js
+++ src/mangrove/datastore/views/reduce_by_values.js
@@ -1,17 +1,13 @@
 function(key, values, rereduce) {
 	if (rereduce == false){
 		result = {};
-		current = values[0];
 		total = 0;
 		count = 0;
 		for(i in values){
 			count = count + 1;
 			x = values[i];
-    			if (x.timestamp > current.timestamp) current = x;
 			if (typeof(x.value)=='number') total = total + x.value;
 		}
-		result.latest = current.value;
-		result.timestamp = current.timestamp;
 		result.sum = total;
         result.count = count;
         result.entity_id = values[0].entity_id;
@@ -21,17 +17,13 @@ function(key, values, rereduce) {
 	}
 	else{
         result = {};
-		current = values[0];
 		total = 0;
 		count = 0;
 		for(i in values){
 			x = values[i];
 			count = count + x.count;
-    			if (x.timestamp > current.timestamp) current = x;
 			if (typeof(x.sum)=='number') total = total + x.sum;
 		}
-        result.latest = current.value;
-        result.timestamp = current.timestamp;
         result.sum = total;
         result.count = count;
         result.entity_id = values[0].entity_id;
