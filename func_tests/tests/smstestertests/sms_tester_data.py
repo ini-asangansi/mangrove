c36b857688dda673f9a9e9f497f4a4d0c17d811a
Mahesh Lal <mahesh.2910@gmail.com>
2011-05-26 15:47:18 +0530

    fixing build <MAhesh>

--- func_tests/tests/smstestertests/sms_tester_data.py
+++ func_tests/tests/smstestertests/sms_tester_data.py
@@ -12,12 +12,12 @@ MESSAGE = "message"
 VALID_DATA = {SENDER: "1234567890",
               RECEIVER: "0987654321",
               SMS: "CLI001 +EID CID003 +NA Mr. Tessy +FA 58 +RD 17.05.2011 +BG b +SY ade",
-              SUCCESS_MESSAGE: "Thank You Shweta for your submission."}
+              SUCCESS_MESSAGE: "Thank you for your submission. We successfully received all answers."}
 
 VALID_DATA2 = {SENDER: "1234567890",
               RECEIVER: "0987654321",
               SMS: "CLI002 +EID CID005 +NA Mr. Tessy +FA 58 +RD 17.05.2011 +BG b +SY ade",
-              SUCCESS_MESSAGE: "Thank You Shweta for your submission."}
+              SUCCESS_MESSAGE: "Thank you for your submission. We successfully received all answers."}
 
 EXCEED_NAME_LENGTH = {SENDER: "1234567890",
               RECEIVER: "0987654321",
