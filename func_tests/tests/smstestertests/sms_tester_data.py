c4514123a0353a6e5ca35b8d195b52ff81835e0c
Ritesh Yadav <yadavr@thoughtworks.com>
2011-05-31 15:57:39 +0530

    Fixing build: Ritesh

--- func_tests/tests/smstestertests/sms_tester_data.py
+++ func_tests/tests/smstestertests/sms_tester_data.py
@@ -12,22 +12,22 @@ MESSAGE = "message"
 SUCCESS_MESSAGE_TEXT = "Thank you Shweta for your data record. We successfully received your submission."
 
 VALID_DATA = {SENDER: "1234567890",
-              RECEIVER: "0987654321",
+              RECEIVER: "261333782943",
               SMS: "CLI001 +EID CID003 +NA Mr. Tessy +FA 58 +RD 17.05.2011 +BG b +SY ade",
               SUCCESS_MESSAGE: SUCCESS_MESSAGE_TEXT}
 
 VALID_DATA2 = {SENDER: "1234567890",
-              RECEIVER: "0987654321",
+              RECEIVER: "261333782943",
               SMS: "CLI002 +EID CID005 +NA Mr. Tessy +FA 58 +RD 17.05.2011 +BG b +SY ade",
               SUCCESS_MESSAGE: SUCCESS_MESSAGE_TEXT}
 
 EXCEED_NAME_LENGTH = {SENDER: "1234567890",
-              RECEIVER: "0987654321",
+              RECEIVER: "261333782943",
               SMS: "CLI001 +EID CID003 +NA Mr. O'brain +FA 58 +RD 17.05.2011 +BG b +SY ade",
               ERROR_MSG: "Error. Invalid Submission. Refer to printed Questionnaire. Resend the question ID and answer for na"}
 
 EXCEED_NAME_LENGTH2 = {SENDER: "1234567890",
-              RECEIVER: "0987654321",
+              RECEIVER: "261333782943",
               SMS: "CLI002 +EID CID005 +NA Mr. O'brain +FA 58 +RD 17.05.2011 +BG b +SY ade",
               ERROR_MSG: "Error. Invalid Submission. Refer to printed Questionnaire. Resend the question ID and answer for na"}
 
