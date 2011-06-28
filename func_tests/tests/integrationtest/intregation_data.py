d06730ce0c2d0b0adbeee1169b8471530027dd55
Ritesh Yadav <yadavr@thoughtworks.com>
2011-06-28 17:49:22 +0530

    Fixed functional test: Ritesh

--- func_tests/tests/integrationtest/intregation_data.py
+++ func_tests/tests/integrationtest/intregation_data.py
@@ -124,20 +124,20 @@ VALID_DATA_FOR_REPORTER = {REPORTER_NAME: "Donald Mouse",
 VALID_SUBJECT_TYPE1 = {ENTITY_TYPE: "Waterpoint", SUCCESS_MESSAGE: "Entity definition successful"}
 VALID_SUBJECT_TYPE2 = {ENTITY_TYPE: "Well", SUCCESS_MESSAGE: "Entity definition successful"}
 
-VALID_DATA_FOR_SUBJECT = {ENTITY_TYPE: "well",
+VALID_DATA_FOR_SUBJECT = {ENTITY_TYPE: "waterpoint",
               AUTO_GENERATE: True,
               SHORT_NAME: "",
-              NAME: "Well Monodova",
+              NAME: "Waterpoint Monodova",
               LOCATION: "Monodova",
               GEO_CODE: "47.411631 28.369885",
-              DESCRIPTION: "This is a Well in monodova",
+              DESCRIPTION: "This is a waterpoint in monodova",
               MOBILE_NUMBER: "3456789012",
-              SUCCESS_MESSAGE: "Registration successful. Subject identification number: wel1."}
+              SUCCESS_MESSAGE: "Registration successful. Subject identification number: wat1."}
 
-VALID_DATA_FOR_PROJECT = {PROJECT_NAME: "well morondava", GEN_RANDOM: False,
+VALID_DATA_FOR_PROJECT = {PROJECT_NAME: "Waterpoint morondava", GEN_RANDOM: False,
               PROJECT_BACKGROUND: "This project is created by functional automation suite.",
               PROJECT_TYPE: "survey",
-              SUBJECT: "well",
+              SUBJECT: "waterpoint",
               REPORT_TYPE: "other subject",
               DEVICES: "sms,smartphone",
               PAGE_TITLE: "Subjects"}
@@ -163,8 +163,8 @@ QUESTIONNAIRE_DATA = {QUESTIONNAIRE_CODE: "WPS01", GEN_RANDOM: False,
 
 VALID_DATA_FOR_SMS = {SENDER: "1234567890",
               RECEIVER: "",
-              SMS: "WPS01 +WID wel1 +wl 598 +DMY 12.04.2011 +MY 04.2011 +MDY 04.12.2011  +wc c +WAN Mr. Tessy +wb ab +GPS 27.178057  -78.007789",
+              SMS: "WPS01 +WID wat1 +wl 598 +DMY 12.04.2011 +MY 04.2011 +MDY 04.12.2011  +wc c +WAN Mr. Tessy +wb ab +GPS 27.178057  -78.007789",
               SUCCESS_MESSAGE: SUCCESS_MESSAGE_TEXT}
 
-SMS_DATA_LOG = {SMS_SUBMISSION: "True False wel1 598 12.04.2011 04.2011 04.12.2011 c Mr. Tessy ab 27.178057 -78.007789",
+SMS_DATA_LOG = {SMS_SUBMISSION: "True False wat1 598 12.04.2011 04.2011 04.12.2011 c Mr. Tessy ab 27.178057 -78.007789",
             UNIQUE_VALUE: "Mr. Tessy"}
